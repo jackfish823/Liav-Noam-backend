7a6ded9b4d72f8239ce1d0dee574f4dee0799895
Liav Morad <93830477+liavmorad@users.noreply.github.com>
2025-12-13 13:58:50 +0200

    Get post by ID (#4)
    * Add MongoDB connection setup and dotenv configuration
    
    * Add post creation route and Post model
    
    * configured nodemon
    
    * Add route to retrieve a post by ID
    
    ---------
    
    Co-authored-by: Noam <46218256+jackfish823@users.noreply.github.com>

--- controllers/posts.js
+++ controllers/posts.js
@@ -8,9 +8,24 @@ router.post('/', async (req, res) => {
         const {message, sender} = req.body;
         const newPost = new Post({message, sender});
         const savedPost = await newPost.save();
+      
         res.status(201).json(savedPost);
     } catch (error) {
         res.status(409).json({message: error.message});
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({message: 'Post not found'});
+        }
+
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json({message: error.message});
     }
 });
 
