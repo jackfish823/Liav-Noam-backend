48f8ba6716073572869f114309babc2b023349c8
noamguy <noam.guy09@gmail.com>
2026-01-28 14:34:45 +0200

    - added upload image handle - added get image by id - added delete image by id - added populates to when getting image to get the full preview url

---
+++ src/models/Image.ts
@@ -0,0 +1,53 @@
+import mongoose, {Document, Schema} from 'mongoose';
+
+export interface IImage extends Document {
+    filename: string;
+    originalName: string;
+    mimetype: string;
+    size: number;
+    path: string;
+    uploadedBy: mongoose.Types.ObjectId;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+const imageSchema = new Schema<IImage>({
+    filename: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    originalName: {
+        type: String,
+        required: true,
+    },
+    mimetype: {
+        type: String,
+        required: true,
+    },
+    size: {
+        type: Number,
+        required: true,
+    },
+    path: {
+        type: String,
+        required: true,
+    },
+    uploadedBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+}, {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+});
+
+imageSchema.virtual('url').get(function() {
+    const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
+
+    return `${baseUrl}/image/${this._id}`;
+});
+
+export default mongoose.model<IImage>('Image', imageSchema);
