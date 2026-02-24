63604b54626c0ef740304beb8297cf51b52b87e8
liavmorad <liav.morad444@gmail.com>
2026-02-24 22:44:52 +0200

    feat: implement HTTPS support for production and HTTP for development

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import initApp from "./index";
+import https from "https";
+import http from "http";
+import fs from "fs";
 
-const port = process.env.PORT;
 
 initApp().then((app) => {
-    app.listen(port, () => {
-        console.log(`Example app listening at http://localhost:${port}`);
-    });
+    if (process.env.NODE_ENV !== "production") {
+        console.log('development');
+        http.createServer(app).listen(process.env.PORT);
+    } else {
+        console.log('PRODUCTION');
+        const options = {
+            key: fs.readFileSync("/etc/ssl/private/client-key.pem"),
+            cert: fs.readFileSync("/etc/ssl/certs/client-cert.pem"),
+        }
+        https.createServer(options, app).listen(process.env.HTTPS_PORT);
+    }
 });
